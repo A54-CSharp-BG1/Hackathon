d320415e2bb9fcc816b6e6e245707f7df4d6948d
milievaiv <mari9fromhell@gmail.com>
2023-09-13 14:56:50 +0300

    Revert "fixed padend"
    This reverts commit 8a6ecfe5a807708670c8a5b4da52e92c45e235cc.

--- Hackaton/Strings/StringHelpers.cs
+++ Hackaton/Strings/StringHelpers.cs
@@ -211,9 +211,9 @@ namespace Hackaton
         public static string PadEnd(string source, int length, char paddingSymbol)
         {
             string output = source;
-            for(int i=source.Length; i<length; i++)
+            for(int i=; i<source.Length+length; i++)
             {
-                output += paddingSymbol;
+                source += paddingSymbol;
             }
             return output;
         }
